edb67521542cedb6f260b9d35fb0fc9600917423
edwinpaindracke-spec <edwinpaindracke@gmail.com>
2025-12-11 09:49:45 +0200

    creating a backround colour and adding a functionality of meanings of word, par of speech

--- DictionaryWebApp/Models/WordEntry.cs
+++ DictionaryWebApp/Models/WordEntry.cs
@@ -4,5 +4,9 @@
     {
         public string Word { get; set; }
         public string Definition { get; set; }
+        public string PartOfSpeech { get; set; }
+        public List<string> Examples { get; set; } = new();
+        public List<string> Synonyms { get; set; } = new();
+        public List<string> Antonyms { get; set; } = new();
     }
 }
